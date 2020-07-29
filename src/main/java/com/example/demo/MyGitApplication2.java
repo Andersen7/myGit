79a869d12c530853622f1bb55345b5d846223dde
murphy <290841422@qq.com>
2020-07-29 22:45:56 +0800

    第二次提交

---
+++ src/main/java/com/example/demo/MyGitApplication2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitApplication2.class, args);
+	}
+
+}
